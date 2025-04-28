75fd8c0f56dd93cdc040555b68cf83505b171d25
Moksleivis <Moksleivis@kitm.local>
2025-04-28 15:14:27 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
+import VemaluVedaras from './assets/components/VemaluVedaras'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="collapse bg-base-100 border-base-300 border">
-  <input type="checkbox" />
-  <div className="collapse-title font-semibold">How do I create an account?</div>
-  <div className="collapse-content text-sm">
-    Click the "Sign Up" button in the top right corner and follow the registration process.
-  </div>
-</div>
+    <VemaluVedaras />
     </>
   )
 }
---
+++ src/assets/components/VemaluVedaras.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"; 
+
+function Vemalas () {
+
+const [x, setX] = useState (0);
+const [y, setY] = useState (0);
+const [z, setZ] = useState (0);
+
+const [ answer, setAnswer] = useState (0)
+
+useEffect (() {
+    let ans = x;
+    ans = ans * y +
+
+}, [x, y, z]);
+
+    return (
+        <>
+        <div className="p-5">
+        <div>
+            <label htmlFor="X">X</label>
+            <input type="number" name="X" id="X" value={x} onChange={(e) => { setX(e.target.value)}} />
+            </div>
+            <div>
+            <label htmlFor="Y">Y</label>
+            <input type="number" name="Y" id="Y" value={y} onChange={(e) => { setY(e.target.value)}} />
+            </div>
+            <div>
+            <label htmlFor="Z">Z</label>
+            <input type="number" name="Z" id="Z" value={z} onChange={(e) => { setZ(e.target.value)}} />
+            </div>
+            </div>
+            <div className="p-5">
+                Atsakymas
+                </div>
+            </>
+    );
+}
+
+export default Vemalas
